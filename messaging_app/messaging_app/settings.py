867f28ff5c6e5e75c4dafba4c117ddb701a91acf
SPaci20 <spaci20@gmail.com>
2025-07-20 13:45:28 +0200

    Project SetUp_1

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
